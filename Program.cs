b51a1662aadf13c1f8a084c903d7573cec8bf596
Gustavo Sampaio <gustavosampaio7@hotmail.com>
2025-02-04 13:46:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Criar um console que solicita um número e retorna a tabuada até o 10
+Console.WriteLine("Digite um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+for (int contador = 1; contador <= 10; contador++)
+{
+    Console.WriteLine("{0}*{1}={2}", numero, contador, numero*contador);
+}
